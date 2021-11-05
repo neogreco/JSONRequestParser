1684e4528ca9e95f566da17b867f3a58228d53af
Emiliano Greco <neogreco32@gmail.com>
2021-11-05 11:14:30 +0100

    Added the JsonRequestParser

---
+++ src/JSONRequestParser.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Utility;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use VDX\Brotli\Exception\BrotliException;
+
+class JSONRequestParser
+{
+    public static function extractJsonFrom(String $encodedData, String $encodingHeader)
+    {
+        
+        // create a log channel
+        $log = new Logger('json_parser.log');
+        $log->pushHandler(new StreamHandler('json_parser.log', Logger::INFO));
+        try {
+            
+            if ($encodingHeader == 'gzip') {
+                $log->info("Gzip compression found!");
+                $decompressed = gzdecode($encodedData);
+            } else if ($encodingHeader == 'br') {
+                $log->info("Brotli compression found!");
+                $decompressed = brotli_uncompress($encodedData);
+            } else {
+                $log->info("Not able to risolve for ".$encodingHeader);
+            }
+            $json = json_decode($decompressed);
+        } catch (Exception $e) {
+            return json_encode(
+                array(
+                    'status' => false,
+                    'error' => $e->getMessage()
+                    )
+                );
+            
+        } catch (BrotliException $e) {
+            return json_encode(
+                array(
+                'status' => false,
+                'error' => $e->getMessage()
+                )
+            );
+        }
+        return $json;
+    }
+}
